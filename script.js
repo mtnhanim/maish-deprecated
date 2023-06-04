0c653d3eb5aeb99b9342e8d8ac7999a46b0cd9e8
mtnhanim <135309951+mtnhanim@users.noreply.github.com>
2023-06-04 14:27:38 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+//Home page name animation
+var text = "Maisha Taheyat";   
+var speed = 40; // Interval between each letter (in milliseconds)
+var i = 0;
+var typewriter = document.querySelector('.name');
+
+function typeWriter() {
+  
+  if (i < text.length) {
+    typewriter.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
+//Home page name animation END
+
+
+//ABout Page Image animation
+const imageUrls = [
+  "pics/mtn1.png",
+  "pics/mtn2.jpg",
+  
+];
+
+const ytUrls = [
+  "https://www.youtube.com/embed/lVg8y-rERlk",
+  "https://www.youtube.com/embed/OnvQggy3Ezw",
+  "https://www.youtube.com/embed/EOxarwd3eTs",
+  "https://www.youtube.com/embed/xVx2jyDPinw",
+];
+
+
+
+const imageElement = document.getElementById("image-frame");
+const ytelement = document.getElementById("yt")
+
+function displayImagesWithAnimation() {
+  let index = 0;
+  setInterval(() => {
+    // Apply animation class to the image
+    imageElement.classList.add("fade-in");
+    
+    // Set the image URL
+    imageElement.src = imageUrls[index];
+    
+    // Increment index or reset to 0 if the last image is reached
+    index = (index + 1) % imageUrls.length;
+    
+    // Remove the animation class after a certain time (adjust the duration as needed)
+    setTimeout(() => {
+      imageElement.classList.remove("fade-in");
+    }, 4000); // Duration in milliseconds
+  }, 6000); // Interval between image changes in milliseconds
+}
+
+displayImagesWithAnimation();
+
+
+
+function displayytWithAnimation() {
+  let index = 0;
+  setInterval(() => {
+    // Apply animation class to the image
+    ytelement.classList.add("fade-in");
+    
+    // Set the image URL
+    ytelement.src = ytUrls[index];
+    
+    // Increment index or reset to 0 if the last image is reached
+    index = (index + 1) % ytUrls.length;
+    
+    // Remove the animation class after a certain time (adjust the duration as needed)
+    setTimeout(() => {
+      ytelement.classList.remove("fade-in");
+    }, 2000); // Duration in milliseconds
+  }, 5000); // Interval between image changes in milliseconds
+}
+
+displayytWithAnimation();
+
+
+//ABout Page Image animation END
+
+
